68ff9157570f616d5bc5f480880cd59bc8c10a3a
rwcuffney <rwcuffney@gmail.com>
2023-03-04 13:01:24 -0500

    getting requirements right

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import requests
-import io
-from transformers import pipeline
-from PIL import Image
-import requests
 
 st.set_page_config(layout="wide")
 
